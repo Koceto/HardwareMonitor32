bd52fa8f16b9f21dff016b61520ca7926fb922ef
Konstantin Savov <konstantin.savov@notionfix.com>
2024-03-18 19:31:21 +0200

    removed code preventing service start

--- HardwareMonitor32/Program.cs
+++ HardwareMonitor32/Program.cs
@@ -5,23 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", false, true);
 
+// Removed because it prevents the registered windows service from starting.
 // Start HWInfo process if not already running
-string? executablePath = builder.Configuration.GetSection("Settings")[nameof(Settings.HWInfoExecutablePath)];
-if (!string.IsNullOrEmpty(executablePath))
-{
-    Process[] processes = Process.GetProcessesByName(executablePath.Split("\\").Last().Replace(".exe", ""));
-    if (processes.Length <= 0)
-    {
-        ProcessStartInfo startInfo = new ProcessStartInfo
-        {
-            FileName = executablePath,
-            UseShellExecute = true,
-            Verb = "runas"
-        };
+//string? executablePath = builder.Configuration.GetSection("Settings")[nameof(Settings.HWInfoExecutablePath)];
+//if (!string.IsNullOrEmpty(executablePath))
+//{
+//    Process[] processes = Process.GetProcessesByName(executablePath.Split("\\").Last().Replace(".exe", ""));
+//    if (processes.Length <= 0)
+//    {
+//        ProcessStartInfo startInfo = new ProcessStartInfo
+//        {
+//            FileName = executablePath,
+//            UseShellExecute = true,
+//            Verb = "runas"
+//        };
 
-        Process.Start(startInfo);
-    }
-}
+//        Process.Start(startInfo);
+//    }
+//}
 
 // Add services to the container.
 builder.Services.AddControllers();
--- HardwareMonitor32/readme.md
+++ HardwareMonitor32/readme.md
@@ -1,4 +1,4 @@
 This is registered as a windows service.
-	-Command in Admin CMD: sc create "ShutDown API" binPath="C:\Git\Projects\TurnOffComputerWebApp\Release\HardwareMonitor32.exe"
+	-Command in Admin CMD: sc create "HardwareMonitor32" binPath="C:\Git\Projects\HardwareMonitor32\Release\HardwareMonitor32.exe"
 
 It should auto-update when published.
